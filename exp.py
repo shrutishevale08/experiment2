32760a12c42b55693d070231b69d8995ae3fd356
shrutika <shrutikasidnale1008@gmail.com>
2025-02-07 10:45:38 +0530

    initial commit

---
+++ exp.py
@@ -0,0 +1,4 @@
+a=int(input("enter any value"))
+b=int(input("enter scenod value"))
+c=a+b
+print(c)
---
+++ exp1.py
@@ -0,0 +1,5 @@
+n=int(input("enter any value="))
+m=int(input("enter any value="))
+p=n+m
+print(p)
+
